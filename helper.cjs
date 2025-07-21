52581bb6041a03998d89fd031f0d276d7756255e
sowmiyanarayani <sowmiya2692001@gmail.com>
2025-07-21 19:29:33 +0530

    Generated the randomId for books and users.

--- helper.cjs
+++ helper.cjs
@@ -4,24 +4,17 @@ const { listOfBooks, usersData } = config;
 const getRandomNumber =(min,max)=> Math.floor(Math.random() * (max - min + 1)) +min;
 const getRandomId = () => getRandomNumber(100000,999999);
 
-const addBookIds = (books) => {
-  const usedBookIds = new Set();
-  return books.map(book => ({
-    id: getRandomId(usedBookIds),
+const addBookIds = () => listOfBooks.map(book => ({
+    id: getRandomId(),
     ...book
   }));
-};
 
-const addUserIds = (users) => {
-  const usedUserIds = new Set();
-  return users.map(user => ({
-    userId: getRandomId(usedUserIds),
+const addUserIds = () => usersData.map(user => ({
+    userId: getRandomId(),
     ...user
   }));
-}
+
   
-  const updatedBooks = addBookIds(listOfBooks);
-  console.table(updatedBooks)
-  
-  const updatedUsers = addUserIds(usersData);
-  console.table(updatedUsers)
+ 
+  console.table(addBookIds())
+  console.table(addUserIds())
